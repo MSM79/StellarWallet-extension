9d41e1662a531dfc1716f26a594a30c72c979b49
msm1379 <mohammadmarandi89@gmail.com>
2022-07-27 20:16:57 +0430

    readme

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,7 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
 import { HashRouter as Router } from 'react-router-dom';
 
 render(
