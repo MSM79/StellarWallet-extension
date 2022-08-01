187b285f44aa909e8704600f0cb409edfbe0c432
Mohammad Marandi <mohammadmarandi89@gmail.com>
2022-08-02 04:19:31 +0430

    create utils component

---
+++ src/popup/pages/Signin/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+export default function Signin() {
+  return (
+    <Layout>
+      <div>signin</div>
+    </Layout>
+  );
+}
