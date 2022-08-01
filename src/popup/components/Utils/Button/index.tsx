c42d9435a4161ccaed612af69203dfa5ca9b93e5
Mohammad Marandi <mohammadmarandi89@gmail.com>
2022-08-02 04:20:15 +0430

    fix Route

---
+++ src/popup/components/Utils/Button/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Btn = {
+  text: string;
+};
+
+export default function Button({ text }: Btn) {
+  return (
+    <button className="bg-gray-500 hover:bg-gray-600 text-white text-lg w-full rounded h-12">
+      {text}
+    </button>
+  );
+}
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -9,12 +9,15 @@ import {
 
 import Home from './pages/Home';
 import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import './global.css';
 
 render(
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
     </Routes>
   </Router>,
   global.document.getElementById('root'),
